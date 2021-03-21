714827cf88714245af28648bddb1c32e3e7a953f
Niki Ewald Zakariassen <nikiewald@hotmail.dk>
2021-03-21 19:45:30 +0100

    Added other fix

--- resources/views/eloquent.php
+++ resources/views/eloquent.php
@@ -9,7 +9,6 @@
 
 /**
  * A helper file for Laravel, to provide autocomplete information to your IDE
- * Generated for Laravel <?= $version ?>.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
